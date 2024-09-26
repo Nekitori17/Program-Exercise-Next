eb012540127dff4b89446264fbd31dddebb00c70
Nekitori Hakari <cuongnguyen286641@gmail.com>
2024-09-26 14:04:15 +0000

    fix(): duplicate sort

--- 1. Tổng và tìm số bé và lớn nhất.py
+++ 1. Tổng và tìm số bé và lớn nhất.py
@@ -7,8 +7,8 @@ try:
   with open("./build/1_sum_min_max.out", "w") as out:
     out.writelines([
       f"Tổng các dòng: {sum(list_hang_1)} | {sum(list_hang_2)}" + "\n",
-      f"Trung bình cộng các dòng là: 1 = {sum(list_hang_1) / len(list_hang_1)} 2 = {sum(list_hang_2) / len(list_hang_2)}" + "\n",
-      f"Các số được sắp xếp: {sorted(list_hang_1)} | {sorted(list_hang_1)}" + "\n",
+      f"Trung bình cộng các dòng là: {sum(list_hang_1) / len(list_hang_1)} | {sum(list_hang_2) / len(list_hang_2)}" + "\n",
+      f"Các số được sắp xếp: {sorted(list_hang_1)} | {sorted(list_hang_2)}" + "\n",
       f"Số lớn và bé nhất (bé/lớn): {min(list_hang_1)}/{max(list_hang_1)} | {min(list_hang_2)}/{max(list_hang_2)}"
     ])
 except Exception as e:
