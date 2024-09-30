3e2ee886b21fbb4299924aa2bf597caff9ab972a
Nekitori17 <cuongnguyen286641@gmail.com>
2024-09-30 20:28:58 +0700

    chore(): update exercise required

--- 3. Sắp xếp mảng.py
+++ 3. Sắp xếp mảng.py
@@ -5,7 +5,8 @@ input_file = open("./src/3_sorting_int_list.inp", "r")
 list_hang_1 = [int(x) for x in input_file.readline().split()]
 list_hang_2 = [int(x) for x in input_file.readline().split()]
 
-
+list_so_1 = sap_xep_so(list_hang_1)
+list_so_2 = sap_xep_so(list_hang_2)
 
 with open("./build/3_sorting_int_list.out", "w") as output_file:
   output_file.write(f"Danh sach duoc sap xep: {" ".join([str(x) for x in sap_xep_so(list_hang_1 + list_hang_2)])}")
